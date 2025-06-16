fcb5ba2ff8359ceef49ef7093f7677984c332e9d
llLucidll <akhil10831@gmail.com>
2025-06-15 21:38:36 -0600

    Designed the Pricing frontend page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function App() {
+import Prices from './pages/Pricing';
+//import Home    from './pages/Home';   // you can stub this for now
 
+const lightTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+export default function App() {
   return (
-    <>
-      hello world
-    </>
-  )
+    <ThemeProvider theme={lightTheme}>
+      <Prices />
+    </ThemeProvider>
+  );
 }
-
-export default App
