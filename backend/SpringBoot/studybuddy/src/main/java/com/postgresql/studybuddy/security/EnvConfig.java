23ca50bc4e4ed7c0a36c0552c0b130743d08bada
Snehal-Ray <snehal1@ualberta.ca>
2025-07-24 11:11:21 -0600

    Revert to empty main branch

--- backend/SpringBoot/studybuddy/src/main/java/com/postgresql/studybuddy/security/EnvConfig.java
+++
@@ -1,9 +0,0 @@
-package com.postgresql.studybuddy.security;
-import io.github.cdimascio.dotenv.Dotenv;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class EnvConfig {
-    public static final Dotenv dotenv = Dotenv.load(); // loads .env from root
-}
